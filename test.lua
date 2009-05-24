feb989c20a9a18fe8c9b8a1247f5016f4d206633
Jerome Vuarand <jerome.vuarand@gmail.com>
2009-05-24 18:47:25 +0200

    Added some serialization test for all types.

--- test.lua
+++ test.lua
@@ -12,6 +12,18 @@ local flags_dict = {
 	flag12 = 1 + 2,
 }
 
+local buffer_string = "Hello World!"
+
+local struct_desc = {
+	{'name', 'cstring'},
+	{'value', 'uint32', 'le'},
+}
+
+local fstruct_closure = function(self)
+	self 'name' ('cstring')
+	self 'value' ('uint32', 'le')
+end
+
 assert(serial.serialize.uint8(0x00)==string.char(0x00))
 assert(serial.serialize.uint8(0x55)==string.char(0x55))
 assert(serial.serialize.uint8(0xaa)==string.char(0xaa))
@@ -36,4 +48,46 @@ assert(serial.serialize.flags({flag1=true, flag3=true}, flags_dict, 'uint8')==st
 assert(serial.serialize.flags({flag2=true, flag12=true}, flags_dict, 'uint16', 'le')==string.char(0x03, 0x00))
 
 end
+
+assert(serial.serialize.sizedbuffer(buffer_string, 'uint8')==string.char(#buffer_string)..buffer_string)
+
+assert(serial.serialize.array({42, 37}, 2, 'uint8')==string.char(42, 37))
+assert(serial.serialize.array({0xdead, 0xbeef, 0xd00d, 0xface}, 4, 'uint16', 'be')==string.char(0xde, 0xad, 0xbe, 0xef, 0xd0, 0x0d, 0xfa, 0xce))
+
+assert(serial.serialize.sizedvalue(42, {'uint8'}, 'uint64', 'le')==string.char(8, 42, 0, 0, 0, 0, 0, 0, 0))
+assert(serial.serialize.sizedvalue(buffer_string, {'uint16', 'le'}, 'cstring')==string.char(#buffer_string+1, 0)..buffer_string..'\0')
+assert(serial.serialize.sizedvalue({
+	name = "foo",
+	value = 42,
+}, {'uint8'}, 'struct', struct_desc)==string.char(8).."foo"..'\0'..string.char(42, 0, 0, 0))
+
+assert(serial.serialize.sizedarray({42, 37}, {'uint16', 'le'}, 'uint8')==string.char(2, 0, 42, 37))
+
+assert(serial.serialize.cstring(buffer_string)==buffer_string..'\0')
+
+if pcall(require, 'struct') then
+
+assert(serial.serialize.float(42.37)==string.char(0xe1, 0x7a, 0x29, 0x42))
+
+end
+
+assert(serial.serialize.bytes(buffer_string, #buffer_string)==buffer_string)
+
+assert(serial.serialize.bytes2hex("deadbeef", 4)==string.char(0xde, 0xad, 0xbe, 0xef))
+
+assert(serial.serialize.bytes2base32("deadbeef", 5)==string.char(0x19, 0x00, 0x30, 0x90, 0x85))
+
+assert(serial.serialize.boolean8(false)==string.char(0x00))
+assert(serial.serialize.boolean8(true)==string.char(0x01))
+
+assert(serial.serialize.struct({
+	name = "foo",
+	value = 42,
+}, struct_desc)=="foo"..'\0'..string.char(42, 0, 0, 0))
+
+assert(serial.serialize.fstruct({
+	name = "foo",
+	value = 42,
+}, fstruct_closure)=="foo"..'\0'..string.char(42, 0, 0, 0))
+
 
